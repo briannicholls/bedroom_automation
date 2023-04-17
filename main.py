aa5b077605d18b1561e9020949333634762dafca
Brian Nicholls <nichol88@gmail.com>
2023-04-16 20:59:20 -0400

    assign switch callbacks in main program

--- main.py
+++ main.py
@@ -26,12 +26,20 @@ blinds_state = 0 # 1=open, 0=closed
 fan_power_button.set_on_press_callback(activate_solenoid)
 
 # Set the on_press / on_release callbacks for the manual blinds buttons
-blinds_open_button.set_on_press_callback(servo_motor.open())
-blinds_close_button.set_on_press_callback(servo_motor.close())
-blinds_open_button.set_on_release_callback(servo_motor.stop())
-blinds_close_button.set_on_release_callback(servo_motor.stop())
+blinds_open_button.set_on_press_callback(servo_motor.open)
+blinds_close_button.set_on_press_callback(servo_motor.close)
+blinds_open_button.set_on_release_callback(servo_motor.stop)
+blinds_close_button.set_on_release_callback(servo_motor.stop)
 
 # Set On/Off callbacks for the photoresistor daylight check switch
-daylight_check_switch.set_on_callback(photoresistor.start_checking_daylight(DAYLIGHT_THRESHOLD, blinds_state, servo_motor))
-daylight_check_switch.set_off_callback(photoresistor.stop_checking_daylight())
+daylight_check_switch.set_on_callback(
+  lambda: photoresistor.start_checking_daylight(
+    DAYLIGHT_THRESHOLD,
+    blinds_state,
+    servo_motor
+  )
+)
+daylight_check_switch.set_off_callback(
+  photoresistor.stop_checking_daylight
+)
 
