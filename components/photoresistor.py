def50eec7867f986d37b6bcb85e2aada86cc3a21
Brian Nicholls <nichol88@gmail.com>
2023-04-16 20:42:05 -0400

    refactor photoresistor

--- components/photoresistor.py
+++ components/photoresistor.py
@@ -2,6 +2,8 @@ from machine import ADC, Pin
 import utime
 import _thread
 
+INTERVAL = 10 * 60  # 10 minutes
+
 class Photoresistor:
   def __init__(self, pin) -> None:
     self.control = ADC(Pin(pin))
@@ -16,23 +18,23 @@ class Photoresistor:
     is_daylight = photoresistor_value < daylight_threshold
 
     if is_daylight and blinds_state == 0:
-        print('opening blinds')
-        self.servo.open_blinds()
+      self.servo.open_blinds()
     elif not is_daylight and blinds_state == 1:
-        print('closing blinds')
-        self.servo.close_blinds()
+      self.servo.close_blinds()
 
   def _daylight_loop(self, daylight_threshold, blinds_state):
-      while self.is_checking_daylight:
-          self.check_daylight(daylight_threshold, blinds_state)
-          utime.sleep(10 * 60)  # Sleep for 10 minutes
+    while self.is_checking_daylight:
+      self.check_daylight(daylight_threshold, blinds_state)
+      utime.sleep(INTERVAL)
 
+  # Starts a separate thread to check for daylight,
+  # and trigger given servo to open/close blinds
   def start_checking_daylight(self, daylight_threshold, blinds_state, servo):
-      print('starting daylight check')
-      self.is_checking_daylight = True
-      self.servo = servo
-      _thread.start_new_thread(self._daylight_loop, (daylight_threshold, blinds_state))
+    self.is_checking_daylight = True
+    self.servo = servo
+    _thread.start_new_thread(self._daylight_loop, (daylight_threshold, blinds_state))
 
+  # Stops the thread that checks for daylight
   def stop_checking_daylight(self):
-      print('stopping daylight check')
-      self.is_checking_daylight = False
+    print('stopping daylight check')
+    self.is_checking_daylight = False
