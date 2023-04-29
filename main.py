80584a08b7af2c111dbfe3346c748fdbaea20d1f
Brian Nicholls <nichol88@gmail.com>
2023-04-29 14:31:07 -0400

    clean up / refactor

--- main.py
+++ main.py
@@ -3,40 +3,45 @@ from constants import *
 
 # Import all hardware components
 from components.index import (
-  photoresistor,
-  solenoid_relay,
-  daylight_check_switch,
-  fan_power_button,
-  servo_motor,
-  pir_sensor,
-  blinds_open_button,
-  blinds_close_button
+    photoresistor,
+    solenoid_relay,
+    daylight_check_switch,
+    fan_power_button,
+    servo_motor,
+    pir_sensor,
+    blinds_open_button,
+    blinds_close_button,
 )
+
 
 # Solenoid sequence for pressing the fan power button
 def activate_solenoid():
-  solenoid_relay.value(1)
-  utime.sleep(0.5)
-  solenoid_relay.value(0)
+    solenoid_relay.value(1)
+    utime.sleep(0.5)
+    solenoid_relay.value(0)
 
-# Set the on_press callback for the manual fan button
+
+###############
+# Main Program
+###############
+
+# Set up triggers and callback functions for all switches and buttons
+
+#### Button Callbacks ####
+
+# Manual fan button - activate solenoid on press
 fan_power_button.set_on_press_callback(activate_solenoid)
 
-# Set the on_press / on_release callbacks for the manual blinds buttons
+# Manual blinds buttons - open/close blinds on press, stop on release
 blinds_open_button.set_on_press_callback(servo_motor.open)
 blinds_close_button.set_on_press_callback(servo_motor.close)
 blinds_open_button.set_on_release_callback(servo_motor.stop)
 blinds_close_button.set_on_release_callback(servo_motor.stop)
 
-# Set On/Off callbacks for the photoresistor daylight check switch
+# Photoresistor daylight check switch - start/stop checking daylight on switch
 daylight_check_switch.set_on_callback(
-  lambda: photoresistor.start_checking_daylight(
-    DAYLIGHT_THRESHOLD,
-    blinds_state,
-    servo_motor
-  )
+    lambda: photoresistor.start_checking_daylight(
+        DAYLIGHT_THRESHOLD, blinds_state, servo_motor
+    )
 )
-daylight_check_switch.set_off_callback(
-  photoresistor.stop_checking_daylight
-)
-
+daylight_check_switch.set_off_callback(photoresistor.stop_checking_daylight)
